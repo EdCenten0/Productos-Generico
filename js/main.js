4c5403a13d5797b9202bab0de1dc6324aeab5267
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-06-27 18:06:14 -0600

    I changed the main banner css
    The img was made using before pseudoclass, but it wouldn't work using diferent sections. Now using an img html tag works now using the same properties that i used on before pseudoclass

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 //main.js
 import { getProducts } from "./products.js";
 
-getProducts()
-  .then(products => {
-    console.log(products);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// const list__track = document.querySelector('.main__banner_section__carousel__list__track');
+// const list__track__product = document.querySelector('.main__banner_section__carousel__list__track__product')
+
+
+
+// getProducts()
+//   .then(products => {
+//     console.log(products);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+
+// function assembleProduct() {
+//     let allProducts = []
+
+//     getProducts()
+//         .then(products => {
+//             allProducts = products
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         });
+
+
+//     console.log(allProducts)
+    
+// }
+
+// assembleProduct();
+
