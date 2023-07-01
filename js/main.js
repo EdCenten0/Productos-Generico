a889b1224eb0c2c68ef8ba0e34ec508ff805dc4a
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-06-30 20:06:07 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@
 import { getProducts } from "./data/products.js";
 
 let productos = await getProducts()
-    .then((APIproducts) => {return APIproducts})
+    .then((APIproducts) => {
+        return APIproducts
+    })
     .catch((err) => console.log(err));
 
-let assembleProducts = () => {
-
+const assembleProducts = () => {
+    for(let i = 0; i < 15; i++){
+        
+    }
 }
