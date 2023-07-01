302201536e6de16f040f9fa1f49c85a1ca917d96
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-07-01 10:25:51 -0600

    get users coded

---
+++ js/data/users.js
@@ -0,0 +1,31 @@
+import fetch from "node-fetch"
+
+const API =  'https://api.escuelajs.co/api/v1'
+
+function fetchData(urlAPI){
+    return fetch(urlAPI);
+}
+
+export async function getUsers(){
+    return new Promise((resolve, reject) => {
+        fetchData(`${API}/users`)
+            .then((response) => response.json())
+            .then((users) => resolve(users))
+            .catch((err) => reject(err));
+    });
+}
+
+export async function getOneUser(userId){
+    return new Promise((resolve, reject) =>{
+        fetchData(`${API}/users/${userId}`)
+            .then((response) => response.json())
+            .then((user) => response(user))
+            .catch((err) => reject(err));
+    });
+}
+
+
+
+getUsers()
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err));
