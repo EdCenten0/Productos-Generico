a144dc70274d2b4e7f6297d8f122b618e149ae6d
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-07-12 22:02:09 -0600

    Added event to make appear user popup window

--- js/main.js
+++ js/main.js
@@ -2,67 +2,99 @@
 import { getProducts } from "./data/products.js";
 import { getCategories } from "./data/categories.js";
 
+const header__nav__profile = document.querySelector(".header__nav__profile");
+const header__nav__profile__logo__popup = document.querySelector(
+  ".header__nav__profile__logo__popup"
+);
+
+header__nav__profile.addEventListener("click", () => {
+  header__nav__profile__logo__popup.classList.toggle("inactive");
+});
+
 let products = await getProducts()
-                .then((APIproducts) => {
-                    return APIproducts
-                })
-                .catch((err) => console.log(err));
+  .then((APIproducts) => {
+    return APIproducts;
+  })
+  .catch((err) => console.log(err));
 
 let categories = await getCategories()
-                .then((APIcategories) => {
-                    return APIcategories
-                })
-                .catch((err) => console.log(err));
+  .then((APIcategories) => {
+    return APIcategories;
+  })
+  .catch((err) => console.log(err));
 
+function assembleProducts() {
+  for (let i = 0; i < 10; i++) {
+    const list__track = document.querySelector(
+      ".main__banner_section__carousel__list__track"
+    );
+    const product = document.createElement("div");
+    const product__container = document.createElement("div");
+    const product__container__link = document.createElement("a");
+    const product__container__link__name = document.createElement("p");
+    const product__container__link__picture = document.createElement("picture");
+    const product__container__link__picture__img =
+      document.createElement("img");
 
-function assembleProducts(){
-    for(let i = 0; i < 10; i++){
-        const list__track = document.querySelector('.main__banner_section__carousel__list__track');
-        const product = document.createElement('div');
-        const product__container = document.createElement('div');
-        const product__container__link = document.createElement('a');
-        const product__container__link__name = document.createElement('p');
-        const product__container__link__picture = document.createElement('picture');
-        const product__container__link__picture__img = document.createElement('img');
+    product.classList.add(
+      "main__banner_section__carousel__list__track__product"
+    );
 
-        product.classList.add('main__banner_section__carousel__list__track__product');
-        
-        product__container__link.setAttribute('src', '#');
-        product__container__link__name.innerText = products[i].title;
-        
-        product__container__link__picture__img.setAttribute('src', products[i].images[1]);
-        
-        product__container__link__picture.append(product__container__link__picture__img);
-        product__container__link.append(product__container__link__name, product__container__link__picture);
-        product__container.append(product__container__link);
-        product.append(product__container);
-        list__track.append(product);
-    }
+    product__container__link.setAttribute("src", "#");
+    product__container__link__name.innerText = products[i].title;
+
+    product__container__link__picture__img.setAttribute(
+      "src",
+      products[i].images[1]
+    );
+
+    product__container__link__picture.append(
+      product__container__link__picture__img
+    );
+    product__container__link.append(
+      product__container__link__name,
+      product__container__link__picture
+    );
+    product__container.append(product__container__link);
+    product.append(product__container);
+    list__track.append(product);
+  }
 }
 
-function assembleCategories(){
-    for(let i = 0; i < 6; i++){
-        const list__track = document.querySelector('.categories');
-        const product = document.createElement('div');
-        const product__container = document.createElement('div');
-        const product__container__link = document.createElement('a');
-        const product__container__link__name = document.createElement('p');
-        const product__container__link__picture = document.createElement('picture');
-        const product__container__link__picture__img = document.createElement('img');
+function assembleCategories() {
+  for (let i = 0; i < 6; i++) {
+    const list__track = document.querySelector(".categories");
+    const product = document.createElement("div");
+    const product__container = document.createElement("div");
+    const product__container__link = document.createElement("a");
+    const product__container__link__name = document.createElement("p");
+    const product__container__link__picture = document.createElement("picture");
+    const product__container__link__picture__img =
+      document.createElement("img");
 
-        product.classList.add('main__banner_section__carousel__list__track__product');
-        
-        product__container__link.setAttribute('src', '#');
-        product__container__link__name.innerText = categories[i].name;
-        console.log(categories[i].name)
-        product__container__link__picture__img.setAttribute('src', categories[i].image);
-        
-        product__container__link__picture.append(product__container__link__picture__img);
-        product__container__link.append(product__container__link__name, product__container__link__picture);
-        product__container.append(product__container__link);
-        product.append(product__container);
-        list__track.append(product);
-    }
+    product.classList.add(
+      "main__banner_section__carousel__list__track__product"
+    );
+
+    product__container__link.setAttribute("src", "#");
+    product__container__link__name.innerText = categories[i].name;
+    console.log(categories[i].name);
+    product__container__link__picture__img.setAttribute(
+      "src",
+      categories[i].image
+    );
+
+    product__container__link__picture.append(
+      product__container__link__picture__img
+    );
+    product__container__link.append(
+      product__container__link__name,
+      product__container__link__picture
+    );
+    product__container.append(product__container__link);
+    product.append(product__container);
+    list__track.append(product);
+  }
 }
 
 assembleProducts();
